306bb186e915900764af943a24712fde30fce42a
Praneeth Karnena <praneeth.codes@gmail.com>
2021-03-16 15:55:45 +0530

    Implement signup

--- payments/urls.py
+++ payments/urls.py
@@ -4,5 +4,6 @@ from payments import views
 
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,16 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def home(request):
     return render(request, 'payments/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        _ = User.objects.create_user(username=request.POST['email'], password=request.POST['password'])
+        return HttpResponseRedirect(reverse('login'))
+
+    return render(request, 'payments/signup.html')
