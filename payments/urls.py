9d3d881dfb59e81ba2956c0516d78909de5ad156
Praneeth Karnena <praneeth.codes@gmail.com>
2021-03-17 15:09:35 +0530

    Add payments endpoints

--- payments/urls.py
+++ payments/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.signin, name='login'),
+
+    path('session/', views.create_checkout_session, name='session'),
+    path('checkout/', views.checkout, name='checkout'),
+    
+    path('message/', views.show_message, name='message'),
 ]
